012e1724abf25d9194ad71cf60c862fd6000739a
mfahmifadhs <mfahmifadh2@gmail.com>
2025-02-13 10:08:26 +0700

    Upload

--- app/Imports/SnackcornerImport.php
+++ app/Imports/SnackcornerImport.php
@@ -21,46 +21,49 @@ class SnackcornerImport implements ToModel, WithStartRow
     {
         if (isset($row[10]) || isset($row[8]) || isset($row[7])) {
             $id     = Snackcorner::where('id_snc', $row[10])->first();
-            $satuan = Satuan::select('id_satuan')->where('satuan', 'like', '%' .$row[8]. '%')->first();
+            $satuan = Satuan::select('id_satuan')->where('satuan', 'like', '%' . $row[8] . '%')->first();
             $status   = strtolower($row[9]) == 'tersedia' ? 'true' : 'false';
-        }  else {
+        } else {
             $id     = null;
             $satuan = null;
             $status = null;
         }
 
-        $kategori = SnackcornerKategori::where('nama_kategori', 'like', '%' .$row[2]. '%')->first();
+        $kategori = SnackcornerKategori::where('nama_kategori', 'like', '%' . $row[2] . '%')->first();
         $satuan   = $satuan ? $satuan->id_satuan : null;
         $id       = $id ? $id->id_snc : null;
         $harga    = (strpos($row[6], 'Rp') !== false) ? intval(str_replace([',', 'Rp'], '', $row[6])) : intval($row[6]);
 
-        if ($id) {
-            Snackcorner::where('id_snc', $id)->update([
-                'snc_kategori'   => $kategori->id_kategori,
-                'snc_nama'       => $row[3],
-                'snc_deskripsi'  => $row[4],
-                'snc_satuan'     => $satuan,
-                'snc_maksimal'   => $row[5],
-                'snc_harga'      => $harga,
-                'snc_keterangan' => null,
-                'snc_foto'       => $row[1],
-                'snc_status'     => $status
-            ]);
-        } else {
-            $id_snc = Snackcorner::withTrashed()->count() + 1;
-            $tambah = new Snackcorner();
-            $tambah->id_snc         = $id_snc;
-            $tambah->snc_kategori   = $kategori->id_kategori;
-            $tambah->snc_nama       = $row[3];
-            $tambah->snc_deskripsi  = $row[4];
-            $tambah->snc_satuan     = $satuan;
-            $tambah->snc_maksimal   = $row[5];
-            $tambah->snc_harga      = $harga;
-            $tambah->snc_keterangan = null;
-            $tambah->snc_foto       = $row[2];
-            $tambah->snc_status     = $status;
-            $tambah->created_at     = Carbon::now();
-            $tambah->save();
+        if ($row[3]) {
+            if ($id) {
+                Snackcorner::where('id_snc', $id)->update([
+                    'snc_kategori'   => $kategori->id_kategori,
+                    'snc_nama'       => $row[3],
+                    'snc_deskripsi'  => $row[4],
+                    'snc_satuan'     => $satuan,
+                    'snc_maksimal'   => $row[5],
+                    'snc_harga'      => $harga,
+                    'snc_keterangan' => null,
+                    'snc_foto'       => $row[1],
+                    'snc_status'     => $status
+                ]);
+            } else {
+                dd($row[3]);
+                // $id_snc = Snackcorner::withTrashed()->count() + 1;
+                // $tambah = new Snackcorner();
+                // $tambah->id_snc         = $id_snc;
+                // $tambah->snc_kategori   = $kategori->id_kategori;
+                // $tambah->snc_nama       = $row[3];
+                // $tambah->snc_deskripsi  = $row[4];
+                // $tambah->snc_satuan     = $satuan;
+                // $tambah->snc_maksimal   = $row[5];
+                // $tambah->snc_harga      = $harga;
+                // $tambah->snc_keterangan = null;
+                // $tambah->snc_foto       = $row[1];
+                // $tambah->snc_status     = $status;
+                // $tambah->created_at     = Carbon::now();
+                // $tambah->save();
+            }
         }
     }
 
